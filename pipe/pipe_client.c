c631ef7e1dec26736fb103449c0b40d7e0a498ca
Logan-Developer <logan2.humbert@gmail.com>
2024-03-15 09:38:34 -0600

    pipe: attempt to add windows compatibility

--- pipe/pipe_client.c
+++ pipe/pipe_client.c
@@ -1,37 +1,72 @@
-#include <stdio.h>
+#include <stdio.h> // Common for standard input/output
+
+#ifdef _WIN32
+#include <windows.h>
+#include <tchar.h>
+#define PIPE1 _T("\\\\.\\pipe\\client_to_server.pipe")
+#define PIPE2 _T("\\\\.\\pipe\\server_to_client.pipe")
+#else
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
 #define FIFO1 "client_to_server.pipe"
 #define FIFO2 "server_to_client.pipe"
+#endif
 
 int main() {
     char buffer[1024];
-    int read_fd, write_fd;
+    int bytesRead, bytesWritten; // Use appropriate types based on the OS
 
-    // Open the named pipes
-    write_fd = open(FIFO1, O_WRONLY); // Open client-to-server pipe for writing
-    read_fd = open(FIFO2, O_RDONLY); // Open server-to-client pipe for reading
+    #ifdef _WIN32
+        HANDLE read_handle, write_handle;
+    #else
+        int read_fd, write_fd;
+    #endif
 
-    // Get message from the server
-    read(read_fd, buffer, sizeof(buffer));
-    printf("Server sent: %s\n", buffer);
+    // Pipe Creation (Conditional)
+    #ifdef _WIN32 
+        // ... (Windows named pipe creation code)
+    #else
+        mkfifo(FIFO1, 0666);
+        mkfifo(FIFO2, 0666);
+    #endif 
 
-    // Send a message to the server
-    char* msg = "Hello, I am the client, writen by Logan";
-    write(write_fd, msg, strlen(msg) + 1);
+    // Pipe Opening/Connecting
+    #ifdef _WIN32
+         // ... (Windows code for connecting to named pipes)
+    #else
+        read_fd = open(FIFO1, O_RDONLY);  // Open client-to-server pipe for reading
+        write_fd = open(FIFO2, O_WRONLY); // Open server-to-client pipe for writing
+    #endif
+
+    //  Communication logic (Keep this mostly platform-independent) 
+    #ifdef _WIN32
+        ReadFile(read_handle, buffer, sizeof(buffer), &bytesRead, NULL);
+    #else
+        bytesRead = read(read_fd, buffer, sizeof(buffer)); 
+    #endif
+    printf("Server sent: %s\n", buffer); 
+
+    char* msg = "Hello, I am the client, written by Logan";
+
+    #ifdef _WIN32
+        WriteFile(write_handle, msg, strlen(msg) + 1, &bytesWritten, NULL);
+    #else
+        write(write_fd, msg, strlen(msg) + 1);
+    #endif
     printf("Client sent: %s\n", msg);
 
-    // Read the response from the server
-    read(read_fd, buffer, sizeof(buffer));
-    printf("Server sent: %s\n", buffer);
+   // Closing Pipes/Handles
+   #ifdef _WIN32
+        CloseHandle(read_handle);
+        CloseHandle(write_handle);
+   #else
+        close(read_fd);
+        close(write_fd);
+   #endif
 
-    // Close the pipes
-    close(read_fd);
-    close(write_fd);
     return 0;
 }
--- pipe/pipe_server.c
+++ pipe/pipe_server.c
@@ -1,43 +1,122 @@
-#include <stdio.h>
+#include <stdio.h> // Common for standard input/output
+
+#ifdef _WIN32
+#include <windows.h>
+#include <tchar.h>
+#define PIPE1 _T("\\\\.\\pipe\\client_to_server.pipe")
+#define PIPE2 _T("\\\\.\\pipe\\server_to_client.pipe")
+#else
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-
 #define FIFO1 "client_to_server.pipe"
 #define FIFO2 "server_to_client.pipe"
+#endif
 
-int main()
-{
+int main() {
     char buffer[1024];
-    int read_fd, write_fd;
+    int bytesRead, bytesWritten; // Use appropriate types based on the OS
 
-    // Create the named pipes (if they don't exist)
-    mkfifo(FIFO1, 0666);
-    mkfifo(FIFO2, 0666);
+    #ifdef _WIN32
+        HANDLE read_handle, write_handle;
 
-    // Open the pipes
-    read_fd = open(FIFO1, O_RDONLY);  // Open client-to-server pipe for reading
-    write_fd = open(FIFO2, O_WRONLY); // Open server-to-client pipe for writing
+        // Create the named pipes
+        read_handle = CreateNamedPipe(
+            PIPE1,                 // Pipe name
+            PIPE_ACCESS_INBOUND,   // Read access
+            PIPE_TYPE_MESSAGE,     // Message-type pipe
+            1,                     // Only one instance
+            1024,                  // Outbound buffer size
+            1024,                  // Inbound buffer size 
+            0,                     // Default timeout
+            NULL                   // Default security attributes
+        );
+        if (read_handle == INVALID_HANDLE_VALUE) {
+            _tprintf(_T("Error creating inbound pipe: %d\n"), GetLastError());
+            return 1;
+        }
 
-    // Send a welcome message to the client
+        write_handle = CreateNamedPipe(
+            PIPE2,
+            PIPE_ACCESS_OUTBOUND, 
+            PIPE_TYPE_MESSAGE,
+            1, 
+            1024, 
+            1024, 
+            0,
+            NULL 
+        );
+        if (write_handle == INVALID_HANDLE_VALUE) {
+            _tprintf(_T("Error creating outbound pipe: %d\n"), GetLastError());
+            CloseHandle(read_handle);
+            return 1;
+        }
+
+        // Connect to the inbound pipe (client-to-server)
+        BOOL connected = ConnectNamedPipe(read_handle, NULL);
+        if (!connected) {
+            _tprintf(_T("Error connecting to inbound pipe: %d\n"), GetLastError());
+            CloseHandle(read_handle);
+            CloseHandle(write_handle);
+            return 1;
+        }
+
+        // Connect to the outbound pipe (server-to-client)
+        connected = ConnectNamedPipe(write_handle, NULL);
+        if (!connected) {
+            _tprintf(_T("Error connecting to outbound pipe: %d\n"), GetLastError());
+            CloseHandle(read_handle);
+            CloseHandle(write_handle);
+            return 1;
+        }
+
+    #else // Linux/UNIX
+        int read_fd, write_fd;
+
+        // Create the named pipes (if they don't exist)
+        mkfifo(FIFO1, 0666);
+        mkfifo(FIFO2, 0666);
+
+        // Open the pipes
+        read_fd = open(FIFO1, O_RDONLY);  // Open client-to-server pipe for reading
+        write_fd = open(FIFO2, O_WRONLY); // Open server-to-client pipe for writing
+    #endif
+
+    // Communication Logic (Keep this mostly platform-independent)
     char *helloMsg = "Hello, I am the server!, who are you?";
-    write(write_fd, helloMsg, strlen(helloMsg) + 1);
-
+    #ifdef _WIN32
+        WriteFile(write_handle, helloMsg, strlen(helloMsg) + 1, &bytesWritten, NULL);
+    #else
+        write(write_fd, helloMsg, strlen(helloMsg) + 1);
+    #endif
     printf("Server sent: %s\n", helloMsg);
 
-    // Read from the client
-    read(read_fd, buffer, sizeof(buffer));
+    #ifdef _WIN32
+        ReadFile(read_handle, buffer, sizeof(buffer), &bytesRead, NULL);
+    #else
+        bytesRead = read(read_fd, buffer, sizeof(buffer)); 
+    #endif
     printf("Client sent: %s\n", buffer);
 
-    // send a response to the client
-    char *response = "Nice to meet you, have a good day!";
-    write(write_fd, response, strlen(response) + 1);
+    char *response = "Hello, I am the server, nice to meet you!";
+    #ifdef _WIN32
+        WriteFile(write_handle, response, strlen(response) + 1, &bytesWritten, NULL);
+    #else
+        write(write_fd, response, strlen(response) + 1);
+    #endif
     printf("Server sent: %s\n", response);
 
-    close(read_fd);
-    close(write_fd);
+   // Closing Pipes/Handles
+   #ifdef _WIN32
+        CloseHandle(read_handle);
+        CloseHandle(write_handle);
+   #else
+        close(read_fd);
+        close(write_fd);
+   #endif
+
     return 0;
 }
